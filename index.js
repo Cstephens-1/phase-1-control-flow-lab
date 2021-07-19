d3924051633e7479fc1e010f579c2e1b9409b69e
Cory <corystephens012@gmail.com>
2021-07-19 11:34:51 -0400

    commit tree a931dd3048a47790aba86486b09322918fe2b399

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(n){
+  if(n <= 400){
+    return 'This one is on me!';
+  } else if (n > 2000 && n <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (n > 2500){
+    return 'No can do.'
+  }
+};
+
+function ternaryCheckCity(city){''
+  return (city == "NYC") ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(input){
+  const tip = input;
+  switch (tip){
+    case "generous":
+      return 'Thank you so much.';
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default: 
+      return "Bye.";
+      break;
+  }
 }
